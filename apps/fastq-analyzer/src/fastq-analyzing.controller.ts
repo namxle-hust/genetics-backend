7a8552d198f679e80ac7432d93a0ed036bc81666
namxle <namxle3010@gmail.com>
2023-01-18 23:56:00 +0700

    handling error analyze sample

--- apps/fastq-analyzer/src/fastq-analyzing.controller.ts
+++ apps/fastq-analyzer/src/fastq-analyzing.controller.ts
@@ -1,21 +1,30 @@
 import { RmqService, FASTQ_ANALYZE_EVENT } from '@app/common';
-import { Analysis } from '@app/prisma';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Analysis, AnalysisStatus } from '@app/prisma';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
 import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 import { AnalysisModel } from './models';
-import { FastqAnalyzingService } from './services';
+import { CommunicationService, FastqAnalyzingService } from './services';
 
 @Controller('fastq')
 export class FastqAnalyzingController {
+    private readonly logger = new Logger(FastqAnalyzingController.name)
+
     constructor(
         private readonly fastqAnalyzingService: FastqAnalyzingService,
-        private readonly rmqService: RmqService
+        private readonly rmqService: RmqService,
+        private communicationService: CommunicationService
     ) { }
 
     @EventPattern(FASTQ_ANALYZE_EVENT)
     async getFastqAnalysis(@Payload() data: AnalysisModel, @Ctx() context: RmqContext) {
-        await this.fastqAnalyzingService.analyzeFastq(data);
-        this.rmqService.ack(context)
+        try {
+            await this.fastqAnalyzingService.analyzeFastq(data);
+            this.rmqService.ack(context)
+        } catch (error) {
+            this.logger.error(error)
+            await this.communicationService.updateSampleStatusStatus(AnalysisStatus.ERROR, data.id)
+            this.rmqService.ack(context)
+        }
     }
 
     @Post('test')
