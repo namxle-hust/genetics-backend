acba2915d3a0e4bc8be275d2218e54605603b463
namxle <namxle3010@gmail.com>
2023-01-25 00:37:04 +0700

    update

---
+++ deployment/run/run-vcf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo $AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY > /home/ubuntu/.passwd-s3fs && \
+chmod 600 /home/ubuntu/.passwd-s3fs
+
+s3fs varigenes-storage /home/s3  -o passwd_file=${HOME}/.passwd-s3fs -o allow_other
+
+/bin/bash
