053a26fd05ccc6df85159afc5c4c2eeac092bfe7
namxle <namxle3010@gmail.com>
2023-01-25 15:44:05 +0700

    Update run-vcf.sh

--- deployment/run/run-vcf.sh
+++ deployment/run/run-vcf.sh
@@ -3,6 +3,6 @@
 echo $S3_ACCESS_KEY:$S3_SECRET_KEY > /home/ubuntu/.passwd-s3fs && \
 chmod 600 /home/ubuntu/.passwd-s3fs
 
-s3fs varigenes-storage /home/s3  -o passwd_file=${HOME}/.passwd-s3fs -o allow_other
+s3fs varigenes-storage /home/s3  -o passwd_file=${HOME}/.passwd-s3fs -o allow_other -o umask=000
 
 /bin/bash
