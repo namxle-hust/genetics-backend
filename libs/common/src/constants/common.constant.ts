62b1d4314abf7cd81d70efc7f7de955ba17831bb
namxle <namxle3010@gmail.com>
2023-01-29 12:33:11 +0700

    Update common.constant.ts

--- libs/common/src/constants/common.constant.ts
+++ libs/common/src/constants/common.constant.ts
@@ -1,5 +1,5 @@
 export const FASTQ_OUTPUT_VCF = 'fastq.output.vcf';
 export const FASTQ_OUTPUT_VCF_COMPRESSED = 'fastq.output.vcf.gz'
 
-export const ANNO_FILE = 'analysis.anno'
-export const CANONICAL_FILE = 'analysis.canonical'
+export const RESULT_ANNO_FILE = 'analysis.anno'
+export const RESULT_CANONICAL_FILE = 'analysis.canonical'
