5a0536290fc01b5d2cf7998efdc48ce566a8c1e9
namxle <namxle3010@gmail.com>
2023-02-01 09:51:01 +0700

    Update run-import.sh

--- deployment/run/run-import.sh
+++ deployment/run/run-import.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo $S3_ACCESS_KEY:$S3_SECRET_KEY > /home/ubuntu/.passwd-s3fs && \
-chmod 600 /home/ubuntu/.passwd-s3fs
+HOME=/usr/apps
+
+echo $S3_ACCESS_KEY:$S3_SECRET_KEY > $HOME/.passwd-s3fs && \
+chmod 600 $HOME/.passwd-s3fs
 
 s3fs varigenes-storage /home/s3  -o passwd_file=${HOME}/.passwd-s3fs -o allow_other -o umask=000
 
