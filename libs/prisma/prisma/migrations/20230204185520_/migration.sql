715dfe37b995b52c6bacb35c50c20b431bb8ad1b
namxle <namxle3010@gmail.com>
2023-02-05 02:06:07 +0700

    Update migration.sql

--- libs/prisma/prisma/migrations/20230204185520_/migration.sql
+++ libs/prisma/prisma/migrations/20230204185520_/migration.sql
@@ -2,5 +2,4 @@
 ALTER TABLE "samples" ADD COLUMN     "dob" TEXT,
 ADD COLUMN     "ethnicity" TEXT,
 ADD COLUMN     "firstName" TEXT,
-ADD COLUMN     "gender" "Gender",
 ADD COLUMN     "lastName" TEXT;
