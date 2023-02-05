0bdc800e49a53eab96aad81e7040f0360bb10686
namxle <namxle3010@gmail.com>
2023-02-05 13:15:31 +0700

    test

--- deployment/run/run-vcf.sh
+++ deployment/run/run-vcf.sh
@@ -5,4 +5,5 @@ chmod 600 /home/ubuntu/.passwd-s3fs
 
 s3fs varigenes-storage /home/s3  -o passwd_file=${HOME}/.passwd-s3fs -o allow_other -o umask=000
 
-source $NVM_DIR/nvm.sh && nvm use default && node /home/ubuntu/genetics/dist/apps/vcf-analyzer/main.js 
+source $NVM_DIR/nvm.sh && nvm use default && yarn start:dev vcf-analyzer
+# node /home/ubuntu/genetics/dist/apps/vcf-analyzer/main.js 
