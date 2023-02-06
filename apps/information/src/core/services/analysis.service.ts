459ad657c7068b94c574d440bb53fc0862cd010a
namxle <namxle3010@gmail.com>
2023-02-07 00:56:07 +0700

    remove logs

--- apps/information/src/core/services/analysis.service.ts
+++ apps/information/src/core/services/analysis.service.ts
@@ -19,7 +19,7 @@ export class AnalysisService {
 
         let findInput: IAnalysisFindInput = new AnalysisFindInput(dto, userId);
 
-        this.logger.debug(JSON.stringify(findInput));
+        // this.logger.debug(JSON.stringify(findInput));
 
         let tableFindDto = new TableFindInput<IAnalysisFindInput, AnalysisFilterDTO>(dto, findInput);
 
