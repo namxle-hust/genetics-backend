94a4e6e9185bad0e88434cca553d304e87cdba1b
namxle <namxle3010@gmail.com>
2023-02-06 22:13:07 +0700

    Create test.sh

---
+++ vcf/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sampleId=$1
+
+nohup node vcf/run.js /home/s3/analyses/${sampleId}/analysis.vcf ${sampleId} /home/ubuntu/info/tmp/analysis_${sampleId}_vep.canonical >  /home/ubuntu/info/logs/test.log 2&>1
+
