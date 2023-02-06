2de059c4ba930db2acf9efe34dd0feb40cae658c
namxle <namxle3010@gmail.com>
2023-02-06 22:22:57 +0700

    Update test.sh

--- vcf/test.sh
+++ vcf/test.sh
@@ -4,5 +4,5 @@ analysisId=$1
 
 rm -rf /home/ubuntu/info/tmp/analysis_${analysisId}_run*
 
-nohup node vcf/run.js /home/s3/analyses/${analysisId}/analysis.vcf ${analysisId} /home/ubuntu/info/tmp/analysis_${analysisId}_vep.canonical >  /home/ubuntu/info/logs/test.log 2&>1 &
+nohup node vcf/run.js /home/s3/analyses/${analysisId}/analysis.vcf ${analysisId} /home/ubuntu/info/tmp/analysis_${analysisId}_vep.canonical >  /home/ubuntu/info/logs/test.log 2>&1 &
 
